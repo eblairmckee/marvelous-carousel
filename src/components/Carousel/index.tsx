9e03c82cd27128dd37135b3bfc08e690ddd642ba
Blair McKee <eblair.mckee@gmail.com>
2021-06-16 15:32:42 -0500

    switched to png

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,30 +1,8 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import styled from 'styled-components';
-import { ReactComponent as First } from '../../images/1.svg';
-import { ReactComponent as Second } from '../../images/2.svg';
-import { ReactComponent as Third } from '../../images/3.svg';
-import { ReactComponent as Fourth } from '../../images/4.svg';
-import { ReactComponent as Fifth } from '../../images/5.svg';
 
 const Carousel = () => {
 	const [currentSlide, setCurrentSlide] = useState<number>(1);
-
-	const getCurrentSlide = () => {
-		switch (currentSlide) {
-			case 1:
-				return <First />;
-			case 2:
-				return <Second />;
-			case 3:
-				return <Third />;
-			case 4:
-				return <Fourth />;
-			case 5:
-				return <Fifth />;
-			default:
-				<First />;
-		}
-	};
 
 	return (
 		<Wrapper>
@@ -37,7 +15,7 @@ const Carousel = () => {
 			>
 				-
 			</CarouselButton>
-			{getCurrentSlide()}
+			<Slide src={`${currentSlide}.png`} />
 			<CarouselButton
 				onClick={() =>
 					currentSlide === 5
@@ -66,6 +44,10 @@ const CarouselButton = styled.button`
 	border: none;
 	cursor: pointer;
 	font-size: 28px;
+`;
+
+const Slide = styled.img`
+	width: 80vw;
 `;
 
 export default Carousel;
